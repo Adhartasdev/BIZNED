fffbaad0055a37675d596a02df0cb37569d1832d
Adhartasdev <blecollen@icloud.com>
2022-01-08 07:05:04 +0000

    added markers on google maps

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,15 @@
 function initMap() {
  // Update MAP_ID with custom map ID
  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: 52.373660711320234, lng: 4.891594163168155},
     zoom: 8,
     mapId: '589c5b3a2920d2e2'
     });
+
+ new google.maps.Marker({
+    position: {lat: 52.33718138155934, lng: 4.873088734208704},
+    map,
+    title: "Zuidas Business Center",
+    });
 }
+
