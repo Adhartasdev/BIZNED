e3bfbc7422cd1853454cc28b1afc9345954126d7
Adhartasdev <blecollen@icloud.com>
2022-01-08 06:14:59 +0000

    added google maps functionality

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+function initMap() {
+ // Update MAP_ID with custom map ID
+ map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8,
+    mapId: '589c5b3a2920d2e2'
+    });
+};
