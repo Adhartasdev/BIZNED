63f1becfb7cdf3f4d9e0315b82e0c23876f289c0
Adhartasdev <blecollen@icloud.com>
2022-01-08 09:07:21 +0000

    created js files for all cities

---
+++ JS/amsterdam.js
@@ -0,0 +1,31 @@
+function initMap() {
+    // Update MAP_ID with custom map ID
+    map = new google.maps.Map(document.getElementById('map'), {
+       center: {lat: 52.373660711320234, lng: 4.891594163168155},
+       zoom: 8,
+       mapId: '589c5b3a2920d2e2'
+       });
+   
+       const markers =[
+   
+       ]
+   
+    const marker = new google.maps.Marker({
+       position: {lat: 52.33718138155934, lng: 4.873088734208704},
+       map,
+       title: "Zuidas Business Center",
+       });
+   
+    const infowindow = new google.maps.InfoWindow({
+       content: "Zuidas Business Center",
+        });
+   
+    marker.addListener("click", () => {
+           infowindow.open({
+             anchor: marker,
+             map,
+             shouldFocus: false,
+           });
+       });
+   }
+   
